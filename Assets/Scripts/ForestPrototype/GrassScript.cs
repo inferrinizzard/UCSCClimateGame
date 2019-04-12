e947d6702a10817dabd41e472bfe6d7950a783f3
jhusting <joshhusting@gmail.com>
2019-04-12 02:35:24 -0700

    Added placable and sellable trees

---
+++ Assets/Scripts/ForestPrototype/GrassScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassScript : MonoBehaviour
+{
+    public GameObject TreePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetKey("p"))
+        {
+            Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseLocation.z = -.2f;
+            GameObject NewTree = Instantiate(TreePrefab, mouseLocation, transform.rotation) as GameObject;
+        }
+    }
+}
---
+++ Assets/Scripts/ForestPrototype/TreeScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    public float GrowthRate = 8f;
+    private bool bGrown = false;
+    private float GrowthTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!bGrown)
+        {
+            GrowthTime = Mathf.Clamp(GrowthTime + Time.deltaTime, 0f, GrowthRate);
+            float currScale = transform.localScale.x;
+
+            currScale = Mathf.Lerp(.3f, .6f, GrowthTime / GrowthRate);
+            transform.localScale = new Vector3(currScale, currScale, currScale);
+
+            if(GrowthTime >= GrowthRate)
+            {
+                bGrown = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Cloud")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && bGrown)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
