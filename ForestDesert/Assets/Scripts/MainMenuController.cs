09241a15235533da156c2ee787225e253d7aa057
jhusting <joshhusting@gmail.com>
2019-05-14 22:05:45 -0700

    Added overworld map and UI elements

--- ForestDesert/Assets/Scripts/MainMenuController.cs
+++ ForestDesert/Assets/Scripts/MainMenuController.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject SettingsReference;
+    public GameObject InfoReference;
+    public GameObject NotificationReference;
+
+    private bool SettingsOn = false;
+    private bool InfoOn = false;
+    private bool NotificationsOn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +24,30 @@ public class MainMenuController : MonoBehaviour
         
     }
 
+    public void ToggleSettings()
+    {
+        SettingsOn = !SettingsOn;
+
+        if (SettingsReference)
+            SettingsReference.SetActive(SettingsOn);
+    }
+
+    public void ToggleInfo()
+    {
+        InfoOn = !InfoOn;
+
+        if (InfoReference)
+            InfoReference.SetActive(InfoOn);
+    }
+
+    public void ToggleNotifications()
+    {
+        NotificationsOn = !NotificationsOn;
+
+        if (NotificationReference)
+            NotificationReference.SetActive(NotificationsOn);
+    }
+
     public void UpdateTemperature(string s)
     {
         GlobalStatics.Temperature = float.Parse(s);
@@ -25,5 +56,10 @@ public class MainMenuController : MonoBehaviour
     public void ChangeLevel(string name)
     {
         SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
