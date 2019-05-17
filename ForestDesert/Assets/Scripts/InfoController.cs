1825b682e594fb8639f1e901b32fae5b7d6f7f5a
jhusting <joshhusting@gmail.com>
2019-05-17 01:54:14 -0700

    Saving some stuff before unity tries to change my scripts

---
+++ ForestDesert/Assets/Scripts/InfoController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class InfoController : MonoBehaviour
+{
+    float[] tempPred = { 70f, 100f, 135f, 145f, 160f, 165f };
+    public LineRenderer tempLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderAllLines();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RenderAllLines()
+    {
+        RenderLine(tempLine, tempPred);
+    }
+
+    void RenderLine(LineRenderer line, float[] vals)
+    {
+        float start = line.GetComponent<RectTransform>().anchoredPosition.x;
+        float width = GetComponent<RectTransform>().rect.width;
+
+        Debug.Log("Start: " + start + "\nWidth: " + width);
+        line.positionCount = vals.Length;
+        Vector3[] positions = new Vector3[vals.Length];
+
+        for(int i = 0; i < vals.Length; ++i)
+        {
+            positions[i].x = (width - start) * i / (vals.Length - 1);
+            positions[i].y = vals[i];
+            positions[i].z = 1;
+        }
+
+        positions[0].z = 0;
+        line.SetPositions(positions);
+
+        LineScript ls = line.GetComponent<LineScript>();
+        if (ls)
+        {
+            ls.BuildMesh();
+            ls.ChangeValues(vals);
+        }
+    }
+}
---
+++ ForestDesert/Assets/Scripts/LineScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineScript : MonoBehaviour
+{
+    private LineRenderer line;
+
+    private float[] values;
+
+    public void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    public void ChangeValues(float[] n)
+    {
+        values = new float[n.Length];
+
+        for (int i = 0; i < n.Length; ++i)
+            values[i] = n[i];
+    }
+
+    public void BuildMesh()
+    {
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+        Mesh mesh = new Mesh();
+        line.BakeMesh(mesh, true);
+
+        Vector3[] verts = mesh.vertices;
+        Vector3[] newVerts = new Vector3[verts.Length];
+
+        for(int i = 0; i < verts.Length; ++i)
+        {
+            Vector3 curr = verts[i];
+            curr.x = curr.x * 53.5f;
+            curr.y = curr.y * 53.5f;
+            curr.z = curr.z * 53.5f;
+
+            newVerts[i] = curr;
+        }
+
+        mesh.vertices = newVerts;
+
+        meshCollider.sharedMesh = mesh;
+    }
+
+    private Vector3 FindClosestValue(Vector3 v)
+    {
+        float closestDistance = Mathf.Infinity;
+        int closestIndex = -1;
+
+        Vector3[] pos = new Vector3[line.numPositions];
+        line.GetPositions(pos);
+
+        for (int i = 0; i < values.Length; ++i)
+        {
+            float dist = Vector3.Distance(v, pos[i]);
+            if (dist < closestDistance)
+            {
+                closestDistance = dist;
+                closestIndex = i;
+            }
+        }
+
+        if (closestIndex != -1)
+            return pos[closestIndex];
+        else
+            return new Vector3(0, 0, 0);
+    }
+
+    private void OnMouseOver()
+    {
+        Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseLocation.z = 0f;
+
+        Debug.Log(mouseLocation);
+    }
+
+    private void OnMouseEnter()
+    {
+        line.SetWidth(.2f, .2f);
+    }
+
+    private void OnMouseExit()
+    {
+        line.SetWidth(.1f, .1f);
+    }
+}
