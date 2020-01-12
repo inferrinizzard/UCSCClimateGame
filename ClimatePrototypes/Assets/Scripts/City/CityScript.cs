b5ca7173a51b36de150462cfcf8a73118675f471
inferrinizzard <inferrinizzard@gmail.com>
2020-01-12 14:12:51 -0800

    json.net

--- ClimatePrototypes/Assets/Scripts/City/CityScript.cs
+++ ClimatePrototypes/Assets/Scripts/City/CityScript.cs
@@ -4,21 +4,19 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CityScript : MonoBehaviour
-{
+public class CityScript : MonoBehaviour {
 	// Start is called before the first frame update
 	public Text left, right;
 	[Range(0.01f, 0.1f)] public float speed = .1f;
 	List<string> bills = new List<string>();
-	void Start()
-	{
+	void Start() {
+
 		string line, acc = "";
-		System.IO.StreamReader file = new System.IO.StreamReader(@"./Assets/Scripts/bills.txt");        //read bills from file
-		while ((line = file.ReadLine()) != null)
-		{
+		System.IO.StreamReader file = new System.IO.StreamReader(@"./Assets/Scripts/bills.txt"); //read bills from file
+		while ((line = file.ReadLine()) != null) {
 			acc += line + " \n";
 		}
-		acc.Split('#').ToList().ForEach(x => bills.Add(x.TrimStart()));     //split by '#'
+		acc.Split('#').ToList().ForEach(x => bills.Add(x.TrimStart())); //split by '#'
 		StartCoroutine(Typewriter(left, bills[World.billIndex], speed));
 		StartCoroutine(Typewriter(right, bills[World.billIndex + 1], speed));
 	}
@@ -26,17 +24,16 @@ public class CityScript : MonoBehaviour
 	// Update is called once per frame
 	void Update() { }
 
-	IEnumerator Typewriter(Text print, string text, float speed)        //given text to print, text ref, and print speed, does typewriter effect
+	IEnumerator Typewriter(Text print, string text, float speed) //given text to print, text ref, and print speed, does typewriter effect
 	{
 
-		for (int i = 0; i < text.Length - 1; i++)
-		{
+		for (int i = 0; i < text.Length - 1; i++) {
 			print.text = text.Substring(0, i);
 			yield return new WaitForSeconds(speed);
 		}
 	}
 
-	public void nextBill()      //intereates next bill
+	public void nextBill() //intereates next bill
 	{
 		left.text = right.text = "";
 		StopAllCoroutines();
