fbd024103a3e1caa38870e3f08e071c92be367f4
Amber Sargeant <38865439+AmberSargeant@users.noreply.github.com>
2020-01-23 17:12:33 -0800

    Implemented overworld UI Icon Functionlality for Public Opinion

--- ClimatePrototypes/Assets/Scripts/Main/World.cs
+++ ClimatePrototypes/Assets/Scripts/Main/World.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class World {
 	public static float money = 100f;
-	public static float publicOpinion = 5f;
+	public static float publicOpinion = 50f;
 	public static int turn = 1;
 	public static int actionsRemaining = 2;
 	public static double[] temp;
@@ -17,9 +17,9 @@ public static class World {
 
 	public static void Calc(bool useTemp = false, int years = 0, int steps = 0) => (temp, energy, precip) = EBM.Calc(useTemp ? EBM.temp : null, years, steps);
 
-	static void UpdateCO2(double deltaF) => EBM.F += F;
-	static void UpdateMoney(double delta) => money += delta;
-	static void UpdateOpinion(double delta) => publicOpinion += delta;
-	static void UpdateAlbedo(double deltaS1) => EBM.S1 = S1;
+	// static void UpdateCO2(double deltaF) => EBM.F += F;
+	// static void UpdateMoney(double delta) => money += delta;
+	// static void UpdateOpinion(double delta) => publicOpinion += delta;
+	// static void UpdateAlbedo(double deltaS1) => EBM.S1 = S1;
 	// add other albedo?
 }
