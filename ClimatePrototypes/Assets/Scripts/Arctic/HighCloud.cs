01bb2c502ead419d974e09d1ec74f29aa8f634ff
inferrinizzard <inferrinizzard@gmail.com>
2020-02-14 18:25:39 -0800

    clouds

---
+++ ClimatePrototypes/Assets/Scripts/Arctic/HighCloud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighCloud : MonoBehaviour {
+	int heatThreshold = 4;
+	int hits = 0;
+	new SpriteRenderer renderer;
+
+	void Start() {
+		renderer = GetComponent<SpriteRenderer>();
+		print(renderer.color);
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		hits++;
+		renderer.color -= new Color(0, .1f, .1f, 0);
+		if (renderer.color.g < 1 - .1 * heatThreshold) {
+			print("red");
+			renderer.color = Color.white;
+		}
+	}
+}
---
+++ ClimatePrototypes/Assets/Scripts/Arctic/LowCloud.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowCloud : MonoBehaviour {
+	public float brightness = .5f;
+
+	void Start() { }
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (Random.value > brightness) {
+			GetComponent<Collider2D>().enabled = false;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other) => GetComponent<Collider2D>().enabled = true;
+}
