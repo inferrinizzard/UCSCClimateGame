1e363391cacdd9f4caf9610ed9093da6b71c37be
Xueer Zhu <sherryxueerzhu@gmail.com>
2020-02-25 15:37:03 -0800

    Add art asset.

---
+++ ClimatePrototypes/Assets/Scripts/Arctic/FadeBackgroundDay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeBackgroundDay : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _spriteRenderer.color = new Color(1f,1f,1f, Mathf.Lerp(255, 0, 1));
+    }
+}
