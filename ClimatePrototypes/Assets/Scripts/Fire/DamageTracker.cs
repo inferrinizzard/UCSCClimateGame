bcfa1141136d8390db6e35613a2af7740a5e9a96
inferrinizzard <inferrinizzard@gmail.com>
2020-03-19 14:00:40 -0700

    merge fire region

---
+++ ClimatePrototypes/Assets/Scripts/Fire/DamageTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageTracker : MonoBehaviour
+{
+    [SerializeField] Slider damageFire = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        damageFire.value = WaterSpraying.damage;
+    }
+}
--- ClimatePrototypes/Assets/Scripts/Fire/WaterSpraying.cs
+++ ClimatePrototypes/Assets/Scripts/Fire/WaterSpraying.cs
@@ -4,42 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
-public class WaterSpraying : MonoBehaviour {
+public class WaterSpraying : MonoBehaviour
+{
 	public GameObject particle;
+	public GameObject prompt;
+	public GameObject yes;
 	GameObject cloneWater = null;
 	public static float damage = 0;
 	[SerializeField] Text damageText = default;
 	[SerializeField] Text timerText = default;
 	[SerializeField] Slider waterSlider = default;
 	float timer = 60f;
-
 	public float maxWater = 5000f;
-	float curWater = 5000f;
-
+	public float curWater = 5000f;
+	public float incWater = 500f;
 	public float waterRate = 1f;
+	public GameObject Fill;
 
-	private void Start() {
+	private void Start()
+	{
 		curWater = maxWater;
 		timerText.text = string.Format("{00}", timer);
+		
 	}
-	void Update() {
+	void Update()
+	{
 		timerText.text = string.Format("{00}", Mathf.Floor(timer -= Time.deltaTime));
 		damageText.text = $"Damage: {damage}";
-
 		Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-
 		if (timer > 0)
-			if (cloneWater == null) {
-				if (Input.GetMouseButtonDown(0) && curWater > 0) {
+			if (cloneWater == null)
+			{
+				if (Input.GetMouseButtonDown(0) && curWater > 0)
+				{
 					cloneWater = Instantiate(particle, worldPos, Quaternion.identity);
 				}
-			} else {
-				if (Input.GetMouseButtonUp(0) || curWater <= 0) {
+			}
+			else
+			{
+				if (Input.GetMouseButtonUp(0) || curWater <= 0)
+				{
 					Destroy(cloneWater);
 					cloneWater = null;
 				}
-				if (Input.GetMouseButton(0) && curWater > 0) {
+				if (Input.GetMouseButton(0) && curWater > 0)
+				{
 					curWater -= waterRate;
 					waterSlider.value = curWater / maxWater;
 					// Ray ray = Camera.main.ScreenPointToRay(mousePos);
@@ -49,15 +59,61 @@ public class WaterSpraying : MonoBehaviour {
 				}
 			}
 
-		if (timer < 0 || curWater <= 0) {
+		if (timer < 0 || curWater <= 0)
+		{
 			Destroy(cloneWater);
 			cloneWater = null;
 			World.UpdateFactor("co2", damage / 100);
 		}
+
+		if (curWater <= 0)
+		{
+			EnablePrompt();
+			StartCoroutine("Blink");
+		}
+		//Debug.Log(curWater);
+		//Debug.Log(World.money);*/
 	}
 
-	void OnDrawGizmos() {
+	void OnDrawGizmos()
+	{
 		Gizmos.color = Color.red;
 		Gizmos.DrawSphere(new Vector2(Input.mousePosition.x, Input.mousePosition.y), 1);
 	}
+
+	public void IncreaseWaterAmount()
+	{
+		World.money -= 50f;
+		curWater = incWater;
+		waterSlider.value = curWater / maxWater;
+	}
+
+	public void EnablePrompt()
+	{
+		prompt.SetActive(true);
+		yes.SetActive(true);
+		curWater = incWater;
+		waterSlider.value = curWater / maxWater;
+
+	}
+	public void DisablePrompt()
+	{
+		prompt.SetActive(false);
+		yes.SetActive(false);
+		Fill.SetActive(true);
+		StopCoroutine("Blink");
+	}
+	
+	IEnumerator Blink()
+	{
+		while (true)
+		{
+			Fill.SetActive(false);
+			yield return new WaitForSeconds(0.5f);
+			Fill.SetActive(true);
+			yield return new WaitForSeconds(0.5f);
+		}
+
+	}
+
 }
