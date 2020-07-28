2d36c2fc60042ea678547870a659fe4ce1c82756
inferrinizzard <inferrinizzard@gmail.com>
2020-07-27 17:16:15 -0700

    rotate moon

--- ClimatePrototypes/Assets/Scripts/OverWorld/OverworldController.cs
+++ ClimatePrototypes/Assets/Scripts/OverWorld/OverworldController.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 
 public class OverworldController : MonoBehaviour {
 	[SerializeField] GameObject worldWrapper = default;
+	[SerializeField] Transform moon = default;
+	Transform world;
 	[SerializeField] SpriteRenderer bg = default;
 
 	[HideInInspector] public Material fadeMat;
 
-	void Start() { fadeMat = new Material(Shader.Find("Screen/Fade")); }
+	void Start() {
+		fadeMat = new Material(Shader.Find("Screen/Fade"));
+		world = worldWrapper.transform.GetChild(0);
+
+		StartCoroutine(RotateMoon());
+	}
+
+	IEnumerator RotateMoon() {
+		float moonDist = (world.position - moon.position).magnitude;
+		float alpha = Vector2.Angle(Vector2.right, (Vector2) (moon.position - world.position)) * Mathf.Deg2Rad;
+		SpriteRenderer moonSprite = moon.GetComponent<SpriteRenderer>();
+		UnityEngine.SceneManagement.Scene overworldScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+
+		while (overworldScene.isLoaded) {
+			yield return null;
+			float step = Time.time / 2;
+			float x = moonDist * Mathf.Cos(step) * Mathf.Cos(alpha) - moonDist / 2 * Mathf.Sin(step) * Mathf.Sin(alpha);
+			float y = moonDist * Mathf.Cos(step) * Mathf.Sin(alpha) + moonDist / 2 * Mathf.Sin(step) * Mathf.Cos(alpha);
+
+			moon.transform.position = new Vector2(x, y) + (Vector2) world.position;
+			moon.transform.eulerAngles = Vector3.forward * Mathf.Sin(step) * Mathf.Rad2Deg;
+			if (Mathf.Sin(step) > 0)
+				moonSprite.sortingOrder = 0;
+			else
+				moonSprite.sortingOrder = -2;
+		}
+	}
 
 	public void SendToBottom() {
 		Camera.main.transform.position = Vector3.forward * -10;
